57740925edd3d1a04b02c35ef7186ecdac11d331
to2915ny <strikerlee95@gmail.com>
2019-04-29 00:26:08 +0900

    Somewhat complete

--- instagrapd.c
+++ instagrapd.c
@@ -29,6 +29,9 @@ struct ioutc{
 	}out[11];
 	int state;
 }i;
+char dir[50];
+char ip[30];
+
 char *readFile(char *filename) {
     FILE *f = fopen(filename, "r");
  //   assert(f);
@@ -43,7 +46,7 @@ char *readFile(char *filename) {
 }
 
 void
-child_proc(int conn)
+child_proc(int conn,uint16_t work,uint16_t port)
 {
 	struct student new;
 	int len = 0 ;
@@ -53,39 +56,31 @@ child_proc(int conn)
 	int listen_fd, new_socket ;
         struct sockaddr_in address;
         int addrlen = sizeof(address);
+	int filenum ; 
+	char tc_filenum[2];
+	char tc_file[50];
 
-//	struct inoutc i;
 	while((s= recv(conn,(void*)&stu,sizeof(struct student),0))>0){
 	new.id = stu.id;
 	strcpy(new.password,stu.password);
 	strcpy(new.targetc,stu.targetc);
 
 	}	
-	printf("%s\n",new.password);
 	strcpy(i.targetc,stu.targetc);
-
-// copy .in files to struct 		
-
-	strcpy(i.in[0].input,readFile("./testcase/1.in"));
-	strcpy(i.in[1].input,readFile("./testcase/2.in"));
-	strcpy(i.in[2].input,readFile("./testcase/3.in"));
-        strcpy(i.in[3].input,readFile("./testcase/4.in"));
-	strcpy(i.in[4].input,readFile("./testcase/5.in"));
-        strcpy(i.in[5].input,readFile("./testcase/6.in"));
-	strcpy(i.in[6].input,readFile("./testcase/7.in"));
-        strcpy(i.in[7].input,readFile("./testcase/8.in"));
-	strcpy(i.in[8].input,readFile("./testcase/9.in"));
-        strcpy(i.in[9].input,readFile("./testcase/10.in"));
-
-//	printf("%s\n",i.in[0].input);
-//	printf("%s\n",i.targetc);
+	
+	for (filenum = 1; filenum < 11; filenum++){
+		sprintf(tc_filenum, "%d",filenum);
+		strcpy(tc_file, dir);
+		strcat(tc_file, "/");
+		strcat(tc_file, tc_filenum);
+		strcat(tc_file, ".in");
+		strcpy(i.in[filenum-1].input,readFile(tc_file));
+	}
 //=====================Worker Socket========================//
  	struct sockaddr_in serv_addr;
         int sock_fd ;
-      //  int s, len ;
         char buffer[1024] = {0};
         char * data ;
-
         sock_fd = socket(AF_INET, SOCK_STREAM, 0) ;
         if (sock_fd <= 0) {
                 perror("socket failed : ") ;
@@ -94,8 +89,8 @@ child_proc(int conn)
 
         memset(&serv_addr, '0', sizeof(serv_addr));
         serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(9594);
-        if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        serv_addr.sin_port = htons(work);
+        if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
                perror("inet_pton failed : ") ;
                 exit(EXIT_FAILURE) ;
        }
@@ -117,29 +112,17 @@ child_proc(int conn)
 	//state =0 complete, state = 1 error, state =2 time exceed
 	int count =0;
 	if(i.state == 0){
-//	printf("%s\n",i.out[0].output);
 	
-	if(strcmp(i.out[0].output, readFile("./1.out")) ==0)
-		count++;
-	if(strcmp(i.out[1].output, readFile("./2.out")) ==0)
-                count++;
-	if(strcmp(i.out[9].output, readFile("./10.out")) ==0)
-                count++;
-	if(strcmp(i.out[2].output, readFile("./3.out")) ==0)
-                count++;
-        if(strcmp(i.out[3].output, readFile("./4.out")) ==0)
-                count++;
-	if(strcmp(i.out[4].output, readFile("./5.out")) ==0)
-                count++;
-        if(strcmp(i.out[5].output, readFile("./6.out")) ==0)
-                count++;
-	if(strcmp(i.out[6].output, readFile("./7.out")) ==0)
-                count++;
-	
-        if(strcmp(i.out[7].output, readFile("./8.out")) ==0)
-                count++;
-	if(strcmp(i.out[8].output, readFile("./9.out")) ==0)
-                count++;
+	for (filenum = 1; filenum < 11; filenum++){
+                sprintf(tc_filenum, "%d",filenum);
+                strcpy(tc_file, dir);
+                strcat(tc_file, "/");
+                strcat(tc_file, tc_filenum);
+                strcat(tc_file, ".out");
+                if(strcmp(i.out[filenum-1].output, readFile(tc_file)) ==0)
+	                count++;
+
+        }
 	stu.result = count;
 
 	printf("Number of test cases passed is : %d\n",stu.result);
@@ -161,7 +144,7 @@ child_proc(int conn)
         memset(&address, '0', sizeof(address));
         address.sin_family = AF_INET;
         address.sin_addr.s_addr = INADDR_ANY /* the localhost*/ ;
-        address.sin_port = htons(9119);
+        address.sin_port = htons(port+1);
         if (bind(listen_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
                 perror("bind failed : ");
                 exit(EXIT_FAILURE);
@@ -175,10 +158,7 @@ child_proc(int conn)
 	 new_socket = accept(listen_fd, (struct sockaddr *) &address, (socklen_t*)&addrlen) ;
 
 	while(recv(new_socket,buffer,1024,0)>0){}	
-	printf("received buck!\n");	
 	
-	printf("%s\n",buffer);
-//	strcpy(stu.result,"YOUHavePAssed");
 	if(strcmp(buffer,stu.password)==0){
 		send(new_socket,&stu,sizeof(struct student),0);
 		shutdown(new_socket,SHUT_WR);
@@ -187,7 +167,6 @@ child_proc(int conn)
 	printf("This is the wrong Password!\n");
 		
 	}
-	//send(conn,&i,sizeof(struct student),0);
 
 	}
 	else if(i.state ==1){
@@ -197,97 +176,6 @@ child_proc(int conn)
 	{
 		printf("Time takes too long\n");
 	}
-//	printf("%s\n",i.out[0].output);
-	
-//	strcpy(testtest, i.out[0].output);
-/*	while ( (s = recv(conn, buf, 1023, 0)) > 0 ) {
-		buf[s] = 0x0 ;
-		if (data == 0x0) {
-			data = strdup(buf) ;
-			len = s ;
-		}
-		else {
-			data = realloc(data, len + s + 1) ;
-			strncpy(data + len, buf, s) ;
-			data[len + s] = 0x0 ;
-			len += s ;
-		}
-
-	}
-	printf(">%s\n", data) ;
-	
-	orig = data ;
-	while (len > 0 && (s = send(conn, data, len, 0)) > 0) {
-		data += s ;
-//		len -= s ;
-	}
-	data = orig;
-//	len =0;
-//	printf("%s\n",data);
-	shutdown(conn, SHUT_WR) ;
-//	if (orig != 0x0) 
-//	free(orig) ;
-
-//worker
- 	struct sockaddr_in serv_addr;
-        int sock_fd ;
-      //  int s, len ;
-        //char buffer[1024] = {0};
-       // char * data ;
-
-        sock_fd = socket(AF_INET, SOCK_STREAM, 0) ;
-        if (sock_fd <= 0) {
-                perror("socket failed : ") ;
-                exit(EXIT_FAILURE) ;
-        }
-
-        memset(&serv_addr, '0', sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(9293);
-        if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-                perror("inet_pton failed : ") ;
-                exit(EXIT_FAILURE) ;
-        }
-
-        if (connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-                perror("connect failed : ") ;
-                exit(EXIT_FAILURE) ;
-        }
-
-	//scanf("%s", buffer) ;
-
-        //data = buffer ;
-        //len = strlen(buffer) ;
-//        s = 0 ;
-	printf("%s\n",data);
-        while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
-                data += s ;
-                len -= s ;
-        }
-
-        shutdown(sock_fd, SHUT_WR) ;
-
-//        char buf[1024] ;
-        data = 0x0 ;
-        len = 0 ;
-        while ( (s = recv(sock_fd, buf, 1023, 0)) > 0 ) {
-                buf[s] = 0x0 ;
-                if (data == 0x0) {
-                        data = strdup(buf) ;
-                        len = s ;
-:                }
-                else {
-                        data = realloc(data, len + s + 1) ;
-                        strncpy(data + len, buf, s) ;
-                        data[len + s] = 0x0 ;
-                        len += s ;
-                }
-
-        }
-        printf("from worker >%s\n", data);
-	if (orig != 0x0) 
-	      free(orig) ;
-*/
 }
 
 int 
@@ -295,11 +183,35 @@ main(int argc, char const *argv[])
 { 
 	int listen_fd, new_socket ; 
 	struct sockaddr_in address; 
-	int opt = 1; 
+	int opt; 
 	int addrlen = sizeof(address); 
-
+	uint16_t portnum;
 	char buffer[1024] = {0}; 
-
+	char ipadd[30];
+	char wport[30];
+//	char dir[50];
+	uint16_t portnumworker;
+	while((opt = getopt(argc,argv,"p:w:"))!=-1){
+	switch(opt)
+	{
+		case 'p':
+		portnum = atoi(optarg);
+		break;
+		case 'w':
+		memcpy(ipadd,optarg,30);
+		char *ptr = strtok(ipadd,":");
+		strcpy(ip,ptr);
+		ptr = strtok(NULL,"\n");
+		strcpy(wport,ptr);
+		portnumworker = atoi(wport);
+		break;
+	
+			
+	}
+		
+	
+	}
+	strcpy(dir,argv[5]);
 	listen_fd = socket(AF_INET /*IPv4*/, SOCK_STREAM /*TCP*/, 0 /*IP*/) ;
 	if (listen_fd == 0)  { 
 		perror("socket failed : "); 
@@ -309,7 +221,7 @@ main(int argc, char const *argv[])
 	memset(&address, '0', sizeof(address)); 
 	address.sin_family = AF_INET; 
 	address.sin_addr.s_addr = INADDR_ANY /* the localhost*/ ; 
-	address.sin_port = htons(9059); 
+	address.sin_port = htons(portnum); 
 	if (bind(listen_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
 		perror("bind failed : "); 
 		exit(EXIT_FAILURE); 
@@ -328,7 +240,7 @@ main(int argc, char const *argv[])
 		} 
 
 		if (fork() > 0) {
-			child_proc(new_socket) ;
+			child_proc(new_socket,portnumworker,portnum) ;
 		}
 		else {
 			close(new_socket) ;
--- reader.c
+++ reader.c
@@ -3,10 +3,10 @@
 
 int main(int argc, char const *argv[]) {
 
-	int add,sub,mul, div;
-	int i= atoi(argv[1]);
-	int j =atoi(argv[2]);
-	
+	int add,sub,mul, div,i,j;
+//	int i= atoi(argv[1]);
+//	int j =atoi(argv[2]);
+	scanf("%d %d",&i,&j);	
 	add = i+j;	
 	sub = i-j;
 	mul = i * j;
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[]) {
  	printf("%d - %d = %d\n",i,j,sub); 
  	printf("%d x %d = %d\n",i,j,mul);  
 	printf("%d / %d = %d\n",i,j,div);
-//	wait(1);
+//	while(1);
 //	sleep(8);
 	return 0;
 
